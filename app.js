2c78fc74231a873f37d06377cdcf6e36da21f654
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-27 21:52:13 +0200

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
 const flashCardRoutes = require('./routes/flash');
 // nécessaire pour faire des appels
 const cors = require("cors");
@@ -13,6 +13,9 @@ app.use(cors());
 
 // attention à déclaréer après les méthodes .json et urlencoded
 app.use('/api/v1/flash-cards', flashCardRoutes);
+
+// adresse à utiliser en local =>  localhost:4000
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
