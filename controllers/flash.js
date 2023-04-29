3a60b0efc8cd6e0ba0c21e132d2dece900d18b1b
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-29 18:35:26 +0200

    back to original

--- controllers/flash.js
+++ controllers/flash.js
@@ -1,3 +1,5 @@
+
+
 const { request } = require('express');
 const { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
@@ -6,44 +8,26 @@ const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 });
 
-export const config = {
-  runtime: 'edge',
-};
-
-
 const openai = new OpenAIApi(configuration);
 
 const getQuestions = async (req, res) => {
-  const { body } = req;
-  console.log(body);
+  const{body}= req; 
+  console.log(body); 
   // res.json({msg : body})
-  const messages = [...body];
+  const messages = [...body]
   //   let messages = [{ "role": "system", "content": "tell me a joke" }];
-  // const chatGPT = await openai.createChatCompletion({
-  //   model: 'gpt-3.5-turbo',
-  //   stream: true,
-  //   messages,
-  // });
-    const payload = {
+    const chatGPT = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages,
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      max_tokens: 200,
       stream: true,
-      n: 1,
-    };
+    });
 
+    const chatGPTMessage = chatGPT.data.choices[0].message;
 
+    console.log(chatGPTMessage);
 
-  // const chatGPTMessage = chatGPT.data.choices[0].message;
-
-  // console.log(chatGPTMessage);
-  const stream = await OpenAIStream(payload);
-
-  res.json({ msg: 'déclenché', chatGPTMessage: stream });
+    res.json({msg : 'déclenché', chatGPTMessage : chatGPTMessage});
+ 
 };
 
 const onPrompt = async (req, res) => {
@@ -66,5 +50,5 @@ const onPrompt = async (req, res) => {
 
 module.exports = {
   getQuestions,
-  onPrompt,
+  onPrompt
 };
