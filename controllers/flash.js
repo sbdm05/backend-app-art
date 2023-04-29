87c5cc254fc06628b317e6dbd447b20d397b03e9
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-29 17:42:34 +0200

    add stream

--- controllers/flash.js
+++ controllers/flash.js
@@ -6,25 +6,31 @@ const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 });
 
+export const config = {
+  runtime: 'edge',
+};
+
+
 const openai = new OpenAIApi(configuration);
 
 const getQuestions = async (req, res) => {
-  const{body}= req; 
-  console.log(body); 
+  const { body } = req;
+  console.log(body);
   // res.json({msg : body})
-  const messages = [...body]
+  const messages = [...body];
   //   let messages = [{ "role": "system", "content": "tell me a joke" }];
-    const chatGPT = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages,
-    });
+  const chatGPT = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+  });
 
-    const chatGPTMessage = chatGPT.data.choices[0].message;
+  // const chatGPTMessage = chatGPT.data.choices[0].message;
 
-    console.log(chatGPTMessage);
+  // console.log(chatGPTMessage);
+  const stream = await OpenAIStream(payload);
 
-    res.json({msg : 'déclenché', chatGPTMessage : chatGPTMessage});
- 
+  res.json({ msg: 'déclenché', chatGPTMessage: stream });
 };
 
 const onPrompt = async (req, res) => {
@@ -47,5 +53,5 @@ const onPrompt = async (req, res) => {
 
 module.exports = {
   getQuestions,
-  onPrompt
+  onPrompt,
 };
