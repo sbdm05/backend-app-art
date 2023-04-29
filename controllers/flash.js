2cb7c62a95db3178b758a6805d4190b756bd5250
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-29 20:38:51 +0200

    stream

--- controllers/flash.js
+++ controllers/flash.js
@@ -6,25 +6,78 @@ const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 });
 
+let tempArray = [];
+
 const openai = new OpenAIApi(configuration);
 
 const getQuestions = async (req, res) => {
-  const{body}= req; 
-  console.log(body); 
-  // res.json({msg : body})
-  const messages = [...body]
-  //   let messages = [{ "role": "system", "content": "tell me a joke" }];
-    const chatGPT = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages
+  try {
+    const chatGPT = await openai.createCompletion(
+      {
+        model: 'text-davinci-002',
+        prompt: 'It was the best of times',
+        max_tokens: 100,
+        temperature: 0,
+        stream: true,
+      },
+      { responseType: 'stream' }
+    );
+
+    chatGPT.data.on('data', (data) => {
+      const lines = data
+        .toString()
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+      for (const line of lines) {
+        const message = line.replace(/^data: /, '');
+        if (message === '[DONE]') {
+          const parsed = JSON.parse(message);
+          let temp = parsed.choices[0].text;
+
+          res.json({ message: temp });
+
+          return; // Stream finished
+        }
+        try {
+          const parsed = JSON.parse(message);
+          console.log(parsed, 'parsed');
+          let temp = parsed.choices[0].text;
+
+          // tempArray.push(temp);
+
+          // console.log(tempArray);
+          if (!res.writableEnded) {
+            res.json({ message: temp });
+          }
+          // console.log(temp, 'temp'); // string
+          //res.json({ message: temp });
+        } catch (error) {
+          console.error('Could not JSON parse stream message', message, error);
+        }
+      }
     });
+  } catch (error) {
+    if (error.response?.status) {
+      console.error(error.response.status, error.message);
+      error.response.data.on('data', (data) => {
+        const message = data.toString();
+        try {
+          const parsed = JSON.parse(message);
+          console.error('An error occurred during OpenAI request: ', parsed);
+        } catch (error) {
+          console.error('An error occurred during OpenAI request: ', message);
+        }
+      });
+    } else {
+      console.error('An error occurred during OpenAI request', error);
+    }
+  }
 
-    const chatGPTMessage = chatGPT.data.choices[0].message;
+  process.stdout.write('\n');
 
-    console.log(chatGPTMessage);
+  // const chatGPTMessage = chatGPT.data.choices[0].message;
 
-    res.json({msg : 'déclenché', chatGPTMessage : chatGPTMessage});
- 
+  //console.log(chatGPTMessage);
 };
 
 const onPrompt = async (req, res) => {
@@ -47,5 +100,5 @@ const onPrompt = async (req, res) => {
 
 module.exports = {
   getQuestions,
-  onPrompt
+  onPrompt,
 };
