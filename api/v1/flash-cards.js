d8aa3ba567b61be2c60d00e17de3e97bc05ee682
Sonia-Baibou <s.baibou@ohmycode.io>
2025-06-23 11:03:38 +0200

    add cors header

--- api/v1/flash-cards.js
+++ api/v1/flash-cards.js
@@ -16,8 +16,14 @@ const openai = new OpenAI({
 export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With'
+  );
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
